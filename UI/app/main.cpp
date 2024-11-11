1769aab0238531b1039b46a58bb5753a52810d78
Eviral <flarive@hotmail.com>
2024-11-11 22:21:13 +0100

    Finally libconfig and glfw are working fine as static lib with vcpkg

--- UI/app/main.cpp
+++ UI/app/main.cpp
@@ -10,6 +10,7 @@
 //vcpkg install --triplet x64-windows-static
 //$(SolutionDir)libs\libconfig\build\release\static\libconfig++s.lib
 //$(SolutionDir)libs\glfw\lib-vc2022-64\glfw3_mt.lib
+//vcpkg install --manifest --triplet x64-windows-static
 
 #include "../imgui.h"
 #include "../imgui_internal.h"
