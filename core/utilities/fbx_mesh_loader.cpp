43cae4dc0df12b0dd4b6e50a9179ed2514aaff02
Eviral <flarive@hotmail.com>
2024-12-09 22:25:22 +0100

    fbx import WIP

--- core/utilities/fbx_mesh_loader.cpp
+++ core/utilities/fbx_mesh_loader.cpp
@@ -1,3 +1,50 @@
 #include "fbx_mesh_loader.h"
 
 
+
+
+
+#include <filesystem>
+#include <map>
+
+fbx_mesh_loader::fbx_mesh_loader()
+{
+}
+
+bool fbx_mesh_loader::load_model_from_file(const std::string& filepath)
+{
+    std::filesystem::path dir(std::filesystem::current_path());
+    std::filesystem::path file(filepath);
+    std::filesystem::path fullPath = dir / file;
+
+    std::cout << "[INFO] Loading FBX file " << fullPath.filename() << std::endl;
+
+    auto fullAbsPath = std::filesystem::absolute(fullPath);
+    if (!std::filesystem::exists(fullAbsPath)) {
+        std::cerr << "[ERROR] FBX file not found: " << fullAbsPath << std::endl;
+        return false;
+    }
+
+    std::string inputFile = fullAbsPath.generic_string();
+    
+	// Ignoring certain nodes will only stop them from being processed not tokenised (i.e. they will still be in the tree)
+	//ofbx::LoadFlags flags =
+	//	//		ofbx::LoadFlags::IGNORE_MODELS |
+	//	ofbx::LoadFlags::IGNORE_BLEND_SHAPES |
+	//	//ofbx::LoadFlags::IGNORE_CAMERAS |
+	//	//ofbx::LoadFlags::IGNORE_LIGHTS |
+	//	////		ofbx::LoadFlags::IGNORE_TEXTURES |
+	//	//ofbx::LoadFlags::IGNORE_SKIN |
+	//	//ofbx::LoadFlags::IGNORE_BONES |
+	//	//ofbx::LoadFlags::IGNORE_PIVOTS |
+	//	////		ofbx::LoadFlags::IGNORE_MATERIALS |
+	//	//ofbx::LoadFlags::IGNORE_POSES |
+	//	//ofbx::LoadFlags::IGNORE_VIDEOS |
+	//	////ofbx::LoadFlags::IGNORE_LIMBS |
+	//	////		ofbx::LoadFlags::IGNORE_MESHES |
+	//	//ofbx::LoadFlags::IGNORE_ANIMATIONS;
+
+	////g_scene = ofbx::load((ofbx::u8*)content, file_size, (ofbx::u16)flags);
+
+    return true;
+}
--- core/utilities/fbx_mesh_loader.h
+++ core/utilities/fbx_mesh_loader.h
@@ -1,8 +1,26 @@
 #pragma once
 
-#include "ofbx.h"
+#include "../primitives/triangle.h"
+#include "../textures/image_texture.h"
+#include "../textures/displacement_texture.h"
+#include "../materials/phong_material.h"
+#include "../misc/bvh_node.h"
+#include "../misc/singleton.h"
+
+#include <ofbx.h>
+
+#include <memory>
+#include <string>
+
 
 class fbx_mesh_loader
 {
-};
+public:
+    fbx_mesh_loader();
 
+    bool load_model_from_file(const std::string& filepath);
+
+
+private:
+
+};
